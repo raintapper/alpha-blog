adfef626a8745706f0d5f4c077415cc322114531
Rails Udemy Course <chew.barry@gmail.com>
2016-02-18 15:02:46 +0000

    Added associations and articles and added debuggerview

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
+  has_many :articles
+
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true, uniqueness: { case_sensitive: false },
-  length: { minimum: 3, maximum: 25 }
+            length: { minimum: 3, maximum: 25 }
 
   VALID_EMAIL_REGEX= /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i
+
   validates :email, presence: true, uniqueness: {case_sensitve: false},
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
