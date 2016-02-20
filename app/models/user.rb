05f4682ceeded74ed0ab2f77f22dfbc3fd73cd1a
Rails Udemy Course <chew.barry@gmail.com>
2016-02-20 06:32:50 +0000

    Add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :articles
-
-
+  has_many :articles, dependent: :destroy
+  #this 1 addition, ensures whatever articles users have will be deleted once user is deleted
   before_save { self.email = email.downcase }
 
   validates :username, presence: true, uniqueness: { case_sensitive: false },
